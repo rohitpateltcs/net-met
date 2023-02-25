40f3a50c1953047b04ec884993445d77ab40ca26
Your Name01~ <rohit.tcs077@gmail.com>
2023-02-25 01:56:42 -0800

    card build 1st test

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import CardItems from "@/components/CardItems";
 import React from "react";
 
 const HomePage = () => {
@@ -9,6 +10,20 @@ const HomePage = () => {
           alt=""
           className="w-full h-40 lg:h-52 xl:h-60"
         />
+      </div>
+      <div>
+        <h1>Top Courses</h1>
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+        </div>
       </div>
     </div>
   );
---
+++ components/CardItems.js
@@ -0,0 +1,14 @@
+const CardItem = () => {
+  return (
+    <div>
+      <div className="p-2">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1674929041956-f9fbbbdcc227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
